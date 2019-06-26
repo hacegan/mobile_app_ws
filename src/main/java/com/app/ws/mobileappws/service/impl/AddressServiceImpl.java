ac2300127702ef88d4ae18f9cba1f3f19f941f23
Samet Dağ <samet822@hotmail.com>
2019-06-26 12:23:59 +0300

    Update AddressServiceImpl.java

--- src/main/java/com/app/ws/mobileappws/service/impl/AddressServiceImpl.java
+++ src/main/java/com/app/ws/mobileappws/service/impl/AddressServiceImpl.java
@@ -17,40 +17,40 @@ import com.app.ws.mobileappws.shared.dto.AddressDTO;
 @Service
 public class AddressServiceImpl implements AddressService {
 
-	@Autowired
-	UserRepository userRepository;
+ @Autowired
+ UserRepository userRepository;
 
-	@Autowired
-	AddressRepository addressRepository;
+ @Autowired
+ AddressRepository addressRepository;
 
-	@Override
-	public List<AddressDTO> getAddresses(String userId) {
-		List<AddressDTO> returnValue = new ArrayList<>();
-		ModelMapper modelMapper = new ModelMapper();
+ @Override
+ public List < AddressDTO > getAddresses(String userId) {
+  List < AddressDTO > returnValue = new ArrayList < > ();
+  ModelMapper modelMapper = new ModelMapper();
 
-		UserEntity userEntity = userRepository.findByUserId(userId);
-		if (userEntity == null)
-			return returnValue;
+  UserEntity userEntity = userRepository.findByUserId(userId);
+  if (userEntity == null)
+   return returnValue;
 
-		Iterable<AddressEntity> addresses = addressRepository.findAllByUserDetails(userEntity);
-		for (AddressEntity addressEntity : addresses) {
-			returnValue.add(modelMapper.map(addressEntity, AddressDTO.class));
-		}
+  Iterable < AddressEntity > addresses = addressRepository.findAllByUserDetails(userEntity);
+  for (AddressEntity addressEntity: addresses) {
+   returnValue.add(modelMapper.map(addressEntity, AddressDTO.class));
+  }
 
-		return returnValue;
-	}
+  return returnValue;
+ }
 
-	@Override
-	public AddressDTO getAddress(String addressId) {
-		AddressDTO returnValue = null;
+ @Override
+ public AddressDTO getAddress(String addressId) {
+  AddressDTO returnValue = null;
 
-		AddressEntity addressEntity = addressRepository.findByAddressId(addressId);
+  AddressEntity addressEntity = addressRepository.findByAddressId(addressId);
 
-		if (addressEntity != null) {
-			returnValue = new ModelMapper().map(addressEntity, AddressDTO.class);
-		}
+  if (addressEntity != null) {
+   returnValue = new ModelMapper().map(addressEntity, AddressDTO.class);
+  }
 
-		return returnValue;
-	}
+  return returnValue;
+ }
 
 }
