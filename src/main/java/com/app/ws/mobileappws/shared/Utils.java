0ccfb0fc5914aa61fc58daf16eaecbf2deac91a5
Samet Dağ <samet822@hotmail.com>
2019-06-26 12:25:08 +0300

    Update Utils.java

--- src/main/java/com/app/ws/mobileappws/shared/Utils.java
+++ src/main/java/com/app/ws/mobileappws/shared/Utils.java
@@ -8,25 +8,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class Utils {
 
-	private final Random RANDOM = new SecureRandom();
-	private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+ private final Random RANDOM = new SecureRandom();
+ private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-	public String generateUserId(int length) {// Kaç karakterlik üreteceğimiz bilgisi geliyor.
-		return generateRandomString(length);
-	}
+ public String generateUserId(int length) { // Kaç karakterlik üreteceğimiz bilgisi geliyor.
+  return generateRandomString(length);
+ }
 
-	public String generateAddressId(int length) {// Kaç karakterlik üreteceğimiz bilgisi geliyor.
-		return generateRandomString(length);
-	}
+ public String generateAddressId(int length) { // Kaç karakterlik üreteceğimiz bilgisi geliyor.
+  return generateRandomString(length);
+ }
 
-	private String generateRandomString(int length) {
-		StringBuilder returnValue = new StringBuilder(length);
+ private String generateRandomString(int length) {
+  StringBuilder returnValue = new StringBuilder(length);
 
-		for (int i = 0; i < length; i++) {// length uzunlugunda rastgele alfabeden secip id olusturuyoruz.
-			returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
-		}
+  for (int i = 0; i < length; i++) { // length uzunlugunda rastgele alfabeden secip id olusturuyoruz.
+   returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+  }
 
-		return new String(returnValue);
-	}
+  return new String(returnValue);
+ }
 
 }
