2f705dcddceb569088eab67a56dc4297f21ee60f
Samet Dağ <samet822@hotmail.com>
2019-06-26 12:18:08 +0300

    Update MobileAppWsApplication.java

--- src/main/java/com/app/ws/mobileappws/MobileAppWsApplication.java
+++ src/main/java/com/app/ws/mobileappws/MobileAppWsApplication.java
@@ -10,24 +10,24 @@ import com.app.ws.mobileappws.security.AppProperties;
 @SpringBootApplication
 public class MobileAppWsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MobileAppWsApplication.class, args);
-	}
+ public static void main(String[] args) {
+  SpringApplication.run(MobileAppWsApplication.class, args);
+ }
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+ @Bean
+ public BCryptPasswordEncoder bCryptPasswordEncoder() {
+  return new BCryptPasswordEncoder();
+ }
 
-	@Bean
-	public SpringApplicationContext springApplicationContext() {
-		return new SpringApplicationContext();
-	}
+ @Bean
+ public SpringApplicationContext springApplicationContext() {
+  return new SpringApplicationContext();
+ }
 
-	@Bean // appcontextden get bean diyip alacagımız yapılar icin burada @bean tanımlaması
-			// yapmamız gerekiyor.
-	public AppProperties getAppProperties() {
-		return new AppProperties();
-	}
+ @Bean // appcontextden get bean diyip alacagımız yapılar icin burada @bean tanımlaması
+ // yapmamız gerekiyor.
+ public AppProperties getAppProperties() {
+  return new AppProperties();
+ }
 
 }
